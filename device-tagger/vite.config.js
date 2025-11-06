609b592ffd9f8609facd310086d22e62fe02fb6e
Garrett Gladden <167457717+garrett-gladden@users.noreply.github.com>
2025-11-06 05:26:45 +0000

    Vite commit

--- device-tagger/vite.config.js
+++ device-tagger/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Device-Tagger/', // <- set repo subpath so built assets are served from /Device-Tagger/
   plugins: [react()],
   server: {
     open: true,
